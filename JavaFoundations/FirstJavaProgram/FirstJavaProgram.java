6ef49d6c950bdadfb04d2f4974c9926eaa0e326b
Thomas Moore <tmoorecode@gmail.com>
2022-04-02 15:14:30 -0400

    second commit

---
+++ JavaFoundations/FirstJavaProgram/FirstJavaProgram.java
@@ -0,0 +1,6 @@
+public class FirstJavaProgram {
+    public static void main(String[] args) {
+        System.out.println("My name is Thomas\nI am 100 years old\nMy hometown is Mars");
+        
+    }
+}
