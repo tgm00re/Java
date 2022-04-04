3f7916daf16fd4a6fec0c2a90a69e1ac8935ad72
Thomas Moore <tmoorecode@gmail.com>
2022-04-04 17:52:38 -0400

    calculator

--- JavaOOP/Calculator/src/com/calculator/calculator/CalculatorTest.java
+++ JavaOOP/Calculator/src/com/calculator/calculator/CalculatorTest.java
@@ -4,12 +4,6 @@ public class CalculatorTest {
 
 	public static void main(String[] args) {
 		Calculator calculator = new Calculator();
-		calculator.performOperation(10.5);
-		calculator.performOperation(10.5);
-		calculator.performOperation(10.5);
-		calculator.performOperation(10.5);
-		calculator.performOperation(10.5);
-		calculator.performOperation(10.5);
-		calculator.getResults();
+		
 	}
 }
